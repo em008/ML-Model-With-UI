62d24d7455c2214ec33befb93299261c54970827
em008 <100390334+em008@users.noreply.github.com>
2023-11-03 17:28:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template # Import libraries
 import model_function  # Import ML model function module
 
+# Initialize Flask app
 app = Flask(__name__)
 
+# Default home page
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('index.html')
 
+# Route used for predictions in app UI   
 @app.route('/predict', methods=['POST'])
 def predict_count():
-    data = request.get_json()
-    input_texts = [data['text1'], data['text2']]
-    predictions = model_function.predict_count(input_texts)
-    
-    result = {'prediction': result}
-    return jsonify(result)
+    features = [float(i) for i in request.form.values()]
+    final_features = [np.array(features)]
+    prediction = model_function.predict_count(final_features)
+
+    result = round(prediction[0], 2)
+    return render_template('index.html', prediction_result='CO2 Emission of the vehicle is :{}'.format(result))
 
 if __name__ == '__main__':
     app.run(debug=True)
