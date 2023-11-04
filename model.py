b4ea1049a2fb511ea8edaf94b9d822c82d57a682
em008 <100390334+em008@users.noreply.github.com>
2023-11-04 17:13:09 -0400

    Update model.py

--- model.py
+++ model.py
@@ -1,61 +1,61 @@
-import pandas as pd
-import numpy as np
-import tensorflow as tf
 from tensorflow import keras
+import pandas as pd
 
-# Data Preparation:
-# URL of the website or API endpoint to fetch data from
-url = 'https://fetch-hiring.s3.amazonaws.com/machine-learning-engineer/receipt-count-prediction/data_daily.csv'
+def prediction_model(data):
+    # Feature Engineering:
+    # Extract various features like year, month, day from date data
+    data['Year'] = data['Date'].dt.year
+    data['Month'] = data['Date'].dt.month
 
-# Load data
-data = pd.read_csv(url)
+    # Split Data:
+    # Define features and target variable
+    X = data[['Year', 'Month']]
+    y = data['Receipt_Count']
 
-# Ensure that the "Date" column is in a proper date format
-data['Date'] = pd.to_datetime(data['# Date'])
+    # Split the data into training and testing sets
+    train_size = int(0.8 * len(data))
+    X_train, X_test = X[:train_size], X[train_size:]
+    y_train, y_test = y[:train_size], y[train_size:]
 
-# Feature Engineering:
-# Extract various features like year, month, day from date data
-data['Year'] = data['Date'].dt.year
-data['Month'] = data['Date'].dt.month
+    # Choose a Model:
+    # Build a neural network model
+    model = keras.Sequential([
+        keras.layers.Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
+        keras.layers.Dense(64, activation='relu'),
+        keras.layers.Dense(1)  # Output layer with one neuron for regression
+    ])
 
-# Split Data:
-# Define features and target variable
-X = data[['Year', 'Month']]
-y = data['Receipt_Count']
+    # Compile the model
+    model.compile(optimizer='adam', loss='mean_squared_error')
 
-# Split the data into training and testing sets
-train_size = int(0.8 * len(data))
-X_train, X_test = X[:train_size], X[train_size:]
-y_train, y_test = y[:train_size], y[train_size:]
+    # Convert data to NumPy arrays
+    X_train = X_train.to_numpy()
+    y_train = y_train.to_numpy()
+    X_test = X_test.to_numpy()
+    y_test = y_test.to_numpy()
 
-# Choose a Model:
-# Build a neural network model
-model = keras.Sequential([
-    keras.layers.Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
-    keras.layers.Dense(64, activation='relu'),
-    keras.layers.Dense(1)  # Output layer with one neuron for regression
-])
+    # Training:
+    # Train model using the training data
+    model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test))
 
-# Compile the model
-model.compile(optimizer='adam', loss='mean_squared_error')
+    return model
 
-# Convert data to NumPy arrays
-X_train = X_train.to_numpy()
-y_train = y_train.to_numpy()
-X_test = X_test.to_numpy()
-y_test = y_test.to_numpy()
+# Example usage:
+if __name__ == "__main__":
+    # Data Preparation:
+    # URL of the website or API endpoint to fetch data from
+    url = 'https://fetch-hiring.s3.amazonaws.com/machine-learning-engineer/receipt-count-prediction/data_daily.csv'
 
-# Training:
-# Train model using the training data
-model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test))
+    # Load data
+    data = pd.read_csv(url)
 
-# Prediction and Evaluation:
-# Use the trained model to predict receipt counts for the test set and evaluate its performance
-mse = model.evaluate(X_test, y_test)
-print(f"Mean Squared Error: {mse}")
+    # Ensure that the "Date" column is in a proper date format
+    data['Date'] = pd.to_datetime(data['# Date'])
 
-# Make Predictions:
-# Once the model is trained and evaluated make predictions on future dates by providing the corresponding feature values
-future_date_features = np.array([[2022, 10]])
-predicted_receipt_count = model.predict(future_date_features)
-print(f"Predicted Receipt Count for Future Date: {predicted_receipt_count[0][0]}")
+    # Create and train the model
+    trained_model = prediction_model(data)
+    
+    # Example: Make predictions for future dates
+    future_date_features = pd.DataFrame({'Year': [2022], 'Month': [10]})
+    predicted_receipt_count = trained_model.predict(future_date_features)
+    print(f"Predicted Receipt Count for Future Date: {predicted_receipt_count[0][0]}")
