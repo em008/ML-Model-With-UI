84759b4725d1f62b649dd5ff0e26999de44f6144
em008 <100390334+em008@users.noreply.github.com>
2023-11-05 18:35:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template # Import libraries
 import model_function  # Import ML model function module
 import pandas as pd
+import numpy as np 
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -13,15 +14,13 @@ def home():
 # Route used for predictions in app UI   
 @app.route('/predict', methods=['POST'])
 def predict():
-    # features = [int(i) for i in request.form.values()]
-    # final_features = [np.array(features)]
-    # prediction = model_function.prediction_model(final_features)
+    features = [int(i) for i in request.form.values()]
+    final_features = [np.array(features)]
 
-    form_data = request.form.to_dict()
-    features = pd.DataFrame(form_data, index=[0])
-    prediction = model_function.prediction_model.predict(features)
+    trained_model = model_function.train_count_prediction_model(model_function.data)
+    prediction = model_function.predict_count_for_date(trained_model, final_features)
 
-    result = round(prediction[0], 2)
+    result = round(prediction, 2)
     return render_template('index.html', prediction_result='The scanned receipt count for this date is :{}'.format(result))
 
 if __name__ == '__main__':
