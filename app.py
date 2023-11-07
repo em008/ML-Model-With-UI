6e6dd7c88ae573ad802d0d500e9002ae143214f1
em008 <100390334+em008@users.noreply.github.com>
2023-11-06 19:53:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, render_template # Import libraries
-import model_function  # Import ML model function module
+import model  # Import ML model function module
 import numpy as np 
 
 # Initialize Flask app
@@ -16,8 +16,8 @@ def predict():
     features = [int(i) for i in request.form.values()]
     final_features = [np.array(features)]
 
-    trained_model = model_function.train_count_prediction_model(model_function.data)
-    prediction = model_function.predict_count_for_date(trained_model, final_features)
+    trained_model = model.train_count_prediction_model(model.data)
+    prediction = model.predict_count_for_date(trained_model, final_features)
 
     result = round(prediction, 2)
     return render_template('index.html', prediction_result='The scanned receipt count for this date is :{}'.format(result))
