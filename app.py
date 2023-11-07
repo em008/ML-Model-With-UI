e53732b7d58d22f08f1a26315b94b0efb93f7b3e
em008 <100390334+em008@users.noreply.github.com>
2023-11-06 20:00:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,14 +13,19 @@ def home():
 # Route used for predictions in app UI   
 @app.route('/predict', methods=['POST'])
 def predict():
-    features = [int(i) for i in request.form.values()]
-    final_features = [np.array(features)]
+    try:
+        features = [int(i) for i in request.form.values()]
+        final_features = [np.array(features)]
+    
+        trained_model = model.train_count_prediction_model(model.data)
+        prediction = model.predict_count_for_date(trained_model, final_features)
+    
+        result = round(prediction, 2)
+        return render_template('index.html', prediction_result='The scanned receipt count for this date is :{}'.format(result))
 
-    trained_model = model.train_count_prediction_model(model.data)
-    prediction = model.predict_count_for_date(trained_model, final_features)
-
-    result = round(prediction, 2)
-    return render_template('index.html', prediction_result='The scanned receipt count for this date is :{}'.format(result))
-
+    except Exception as e:
+        error_message = "An error occurred during prediction: {}".format(str(e))
+        return render_template('index.html', prediction_result=error_message)
+        
 if __name__ == '__main__':
     app.run(debug=True)
